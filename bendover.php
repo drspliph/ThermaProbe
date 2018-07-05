fc2b50e728631929c3497e6807c9b402a9d65661
Lars Angus Lyons <lars@lyons.dk>
2018-07-05 11:49:25 +0200

    Test 321

--- bendover.php
+++ bendover.php
@@ -3,24 +3,24 @@ $base_dir = '/sys/bus/w1/devices/';
 $device_folder = glob($base_dir . '28*')[0];
 
 print_r($device_folder);
-foreach ( $device_folder as $where ) {
-    $device_file = $where . '/w1_slave';
-    $data = file($device_file, FILE_IGNORE_NEW_LINES);
+// foreach ( $device_folder as $where ) {
+//     $device_file = $where . '/w1_slave';
+//     $data = file($device_file, FILE_IGNORE_NEW_LINES);
     
-    $temperature = null;
-    if (preg_match('/YES$/', $data[0])) {
-        if (preg_match('/t=(\d+)$/', $data[1], $matches, PREG_OFFSET_CAPTURE)) {
-            $temperature = $matches[1][0] / 1000;
-        }
-    }
+//     $temperature = null;
+//     if (preg_match('/YES$/', $data[0])) {
+//         if (preg_match('/t=(\d+)$/', $data[1], $matches, PREG_OFFSET_CAPTURE)) {
+//             $temperature = $matches[1][0] / 1000;
+//         }
+//     }
     
-    if ($temperature) {
-        echo "Temperature is ${temperature}C\n";
-    } else {
-        echo "Unable to get temperature\n";
-    }
+//     if ($temperature) {
+//         echo "Temperature is ${temperature}C\n";
+//     } else {
+//         echo "Unable to get temperature\n";
+//     }
     
-}
+// }
 
 ?>
 
