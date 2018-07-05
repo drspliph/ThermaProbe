16b13fe1d0af5a0200ddcdbea3ab36487082ca62
Lars Angus Lyons <lars@lyons.dk>
2018-07-05 18:18:33 +0200

    New model

--- reader.php
+++ reader.php
@@ -5,11 +5,11 @@ $results = glob("{/sys/bus/w1/devices/28*}",GLOB_BRACE);
 
 print_r($results);
 foreach ( $results as $dir ) {
-    $file1 = $dir."name";
+    $file1 = $dir."/name";
     $data1 = file($file1, FILE_IGNORE_NEW_LINES);
     echo "File is : ".$data1[0]."\n";
 
-    $file2 = $dir."w1_slave";
+    $file2 = $dir."/w1_slave";
     $data2 = file($file2, FILE_IGNORE_NEW_LINES);
     if ( preg_match('/YES$/', $data2[0] ) ) {
         if ( preg_match('/t=(\d+)$/', $data2[1], $matches, PREG_OFFSET_CAPTURE ) ) {
