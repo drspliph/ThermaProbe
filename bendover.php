d1bae66aa9c538b002ee18ac9a8553fd3e7efac0
Lars Angus Lyons <lars@lyons.dk>
2018-07-05 17:35:03 +0200

    Now working somewhat ?

--- bendover.php
+++ bendover.php
@@ -1,23 +1,26 @@
 <?php
+
+
 $base_dir = '/sys/bus/w1/devices/';
 
-foreach ( glob($base_dir . '28*')[0] as $folder ) { 
-        $device_file = $folder . '/w1_slave';
-    $data = file($device_file, FILE_IGNORE_NEW_LINES);
+$results = glob("{/sys/bus/w1/devices/28*/w1_slave}",GLOB_BRACE);
+
+
+foreach ( $results as $file ) {
+    $data = file($file, FILE_IGNORE_NEW_LINES);
     
     $temperature = null;
-    if (preg_match('/YES$/', $data[0])) {
-        if (preg_match('/t=(\d+)$/', $data[1], $matches, PREG_OFFSET_CAPTURE)) {
+    if ( preg_match('/YES$/', $data[0] ) ) {
+        if ( preg_match('/t=(\d+)$/', $data[1], $matches, PREG_OFFSET_CAPTURE ) ) {
             $temperature = $matches[1][0] / 1000;
         }
     }
     
     if ($temperature) {
         echo "Temperature is ${temperature}C\n";
-    } else {
-        echo "Unable to get temperature\n";
-    }
-    
+}else{
+    echo "Unable to get temperature\n";
+}
 }
 
 ?>
