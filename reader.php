170f76aa57e27974ef12719b11f0fbe3a38812ec
Lars Angus Lyons <lars@lyons.dk>
2018-07-05 18:14:49 +0200

    Fishy

--- reader.php
+++ reader.php
@@ -5,11 +5,12 @@ $results = glob("{/sys/bus/w1/devices/28*}",GLOB_BRACE);
 
 print_r($results);
 foreach ( $results as $dir ) {
-    $file = $dir.name;
-    $data1 = file($file, FILE_IGNORE_NEW_LINES);
+    $file1 = $dir."name";
+    $data1 = file($file1, FILE_IGNORE_NEW_LINES);
     echo "File is : ".$data1[0]."\n";
 
-    $data2 = file($file, FILE_IGNORE_MEM_LINES);
+    $file2 = $dir."w1_slave";
+    $data2 = file($file2, FILE_IGNORE_MEM_LINES);
     if ( preg_match('/YES$/', $data2[0] ) ) {
         if ( preg_match('/t=(\d+)$/', $data2[1], $matches, PREG_OFFSET_CAPTURE ) ) {
             $temp = $matches[1][0] / 1000;
